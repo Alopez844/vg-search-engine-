98a21b95184a2e6b70ce6588dd921dbddbde9a06
unknown <lopez1432.al@gmail.com>
2022-03-29 13:20:28 -0700

    redid login and signup forms with chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 import Games from "./components/Games/Games";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
+    <ChakraProvider>
     <BrowserRouter>
       <div>
         <Navbar />
         <Routes>
           <Route exact path="/profile" element={<Profile/>}/>
           <Route exact path="/games" element={<Games/>}/>
+          <Route exact path="/LoginForm" element={<LoginForm/>}/>
         </Routes>
       </div>
     </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
