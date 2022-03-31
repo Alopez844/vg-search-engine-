4e9500d8376b14a0e22dcc50e431b21edb11191c
sohara <steph.ohara@icloud.com>
2022-03-30 21:03:48 -0700

    added styling to profile and added friend file

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
-import { Image, Box, Badge, Flex } from "@chakra-ui/react";
-import { VStack, Grid, GridItem, StackDivider } from "@chakra-ui/react";
+import {
+  Image,
+  Box,
+  Badge,
+  Flex,
+  Avatar,
+  Text,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  useColorModeValue,
+  VStack,
+  Grid,
+  GridItem,
+  StackDivider,
+  Spacer,
+} from "@chakra-ui/react";
+import { FiBell, FiChevronDown } from "react-icons/fi";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
+import Friend from "./Friend";
 
-const UserImage = ({ pic, name}) => (
-    <Image src={pic || 'https://bit.ly/dan-abramov'} alt={name} boxSize={200}/>
+const UserImage = ({ pic, name }) => (
+  <Image src={pic || "https://bit.ly/dan-abramov"} alt={name} boxSize={200} />
 );
 
 function Profile() {
@@ -15,35 +36,85 @@ function Profile() {
   // const userInfo = data?.me || {};
   // console.log(userInfo);
   return (
-    <Flex>
-        <VStack
-      divider={<StackDivider borderColor="gray.200" />}
-      spacing={100}
-      align="stretch"
-    >
-      <Box h="40px" bg="yellow.200">
-        John Doe
-        <Image
-          boxSize="200px"
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-        <Badge borderRadius="full" px="2" colorScheme="teal">
-          {/* {userInfo.username.toUpperCase()} */}
-        </Badge>
-      </Box>
+    <Flex justifyContent={{ base: "flex-end" }}>
+      <VStack
+        w="100%"
+        divider={<StackDivider borderColor="gray.200" />}
+        spacing={100}
+        align="stretch"
+      >
+        <HStack alignSelf="flex-end" spacing={{ base: "0", md: "6" }}>
+          <IconButton
+            size="lg"
+            variant="ghost"
+            aria-label="open menu"
+            icon={<FiBell />}
+          />
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                py={2}
+                transition="all 0.3s"
+                _focus={{ boxShadow: "none" }}
+              >
+                <HStack>
+                  <Avatar
+                    size={"sm"}
+                    src={
+                      "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                    }
+                  />
+                  <VStack
+                    display={{ base: "none", md: "flex" }}
+                    alignItems="flex-start"
+                    spacing="1px"
+                    ml="2"
+                  >
+                    <Text fontSize="sm">Justina Clark</Text>
+                    <Text fontSize="xs" color="gray.600">
+                      Admin
+                    </Text>
+                  </VStack>
+                  <Box display={{ base: "none", md: "flex" }}>
+                    <FiChevronDown />
+                  </Box>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                bg={useColorModeValue("white", "gray.900")}
+                borderColor={useColorModeValue("gray.200", "gray.700")}
+              >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>Settings</MenuItem>
+                <MenuItem>Billing</MenuItem>
+                <MenuDivider />
+                <MenuItem>Sign out</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </HStack>
 
-      <Box h="40px" bg="white">
-        About Me
-      </Box>
+        <HStack>
+          <Box h="40px" bg="white">
+          <Text fontSize="4xl">Games</Text>
+          </Box>
+        </HStack>
 
-      <Box h="40px" bg="White">
-        Favorite Games
-      </Box>
-    </VStack>
+        <HStack>
+          <Box h="40px" bg="White">
+            <Text fontSize="4xl">Friends</Text>
+            <HStack>
+            <Friend />
+            <Spacer />
+            <Friend />
+            <Spacer />
+            <Friend />
+            </HStack>
+          </Box>
+        </HStack>
+      </VStack>
     </Flex>
   );
 }
 
 export default Profile;
-
